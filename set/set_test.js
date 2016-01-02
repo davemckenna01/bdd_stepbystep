9d1234a901644f4e86c83ca8f9295ee8f84427ef
Dave McKenna <davemckenna01@gmail.com>
2016-01-01 23:53:29 -0500

    initial commit

---
+++ set/set_test.js
@@ -0,0 +1,25 @@
+/*
+How to run tests:
+
+1) > npm install -g mocha
+
+2) > mocha -u bdd -R spec test/
+*/
+
+var expect = require('chai').expect,
+    sinon  = require('sinon'),
+    Set    = require('./set.js').Set;
+
+describe('Set', function(){
+    before(function(){
+    });
+
+    after(function(){
+    });
+
+    describe('', function(){
+        it('', function(){
+        });
+    });
+});
+
