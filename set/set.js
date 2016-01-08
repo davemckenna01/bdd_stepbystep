4d5d62ce3367d33c872329c4fe250b35a37f2442
Dave McKenna <davemckenna01@gmail.com>
2016-01-07 21:29:25 -0500

    add doesn't allow duplicates:green

--- set/set.js
+++ set/set.js
@@ -3,8 +3,23 @@ function Set () {
 }
 
 Set.prototype.add = function(item) {
-    this.members.push(item);
+    var itemIsMember;
+
+    itemIsMember = false;
+
+    for (var i = 0; i < this.members.length; i++) {
+        if (item === this.members[i]) {
+            itemIsMember = true;
+
+            break;
+        }
+    }
+
+    if (!itemIsMember) {
+        this.members.push(item);
+    }    
 }
+
 
 
 exports.Set = Set;
