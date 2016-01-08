1c5a0dce223e0815c666d5283e60cdc6dba88437
Dave McKenna <davemckenna01@gmail.com>
2016-01-07 21:17:57 -0500

    add some possible test descriptions

--- set/set_test.js
+++ set/set_test.js
@@ -6,12 +6,32 @@ How to run tests:
 2) > mocha -u bdd -R spec test/
 */
 
-var expect = require('chai').expect,
+/*
+Problem: we want to create a set data type in Javascript.
+
+Restrict membership to numbers.
+
+Operations:
+add, remove, check for membership
+
+Test list:
+
+set.add() adds a new member to the set
+set.add() doesn't allow duplicates
+set.add() only allows numbers
+
+set.remove() removes a member from the set
+
+set.isMember() checks whether value is a member
+
+*/
+
+var expect = require('chai').should(),
     sinon  = require('sinon'),
     Set    = require('./set.js').Set;
 
 describe('Set', function(){
-    before(function(){
+    beforeEach(function(){
     });
 
     after(function(){
