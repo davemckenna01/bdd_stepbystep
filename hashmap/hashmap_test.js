0a6439b82a8510e0454c5274f9c5afaf21e0beb0
Dave McKenna <davemckenna01@gmail.com>
2016-01-16 23:12:43 -0500

    _hash returns reasonably distributed integers:red

--- hashmap/hashmap_test.js
+++ hashmap/hashmap_test.js
@@ -60,6 +60,52 @@ describe('HashMap', function(){
                 hash.should.be.within(0, 9);
             }
         });
+
+        it('returns reasonably distributed integers', function(){
+            var low, med, high, distribution, numTrials;
+
+            function trial() {
+                var key,
+                    hash,
+                    distribution;
+
+                distribution = [0,0,0,0,0,0,0,0,0,0];
+
+                for (var i = 0; i < 1000; i++) {
+                    key = Math.random().toString(36).slice(2);
+
+                    hash = HashMap._hash(key);
+
+                    distribution[hash]++;
+                }
+
+                distribution.sort(function sortNumber(a,b) {return a - b;});
+
+                // returns something like:
+                // [ 82, 87, 93, 97, 98, 100, 107, 107, 109, 120 ]
+                return distribution;
+            }
+
+            low = med = high = 0;
+
+            numTrials = 15;
+
+            for (var i = 0; i < numTrials; i++) {
+                distribution = trial();
+
+                low += distribution[0];
+                med += distribution[5];
+                high += distribution[9];
+            }
+
+            low = low / numTrials;
+            med = med / numTrials;
+            high = high / numTrials;
+
+            low.should.be.within(80, 90);
+            med.should.be.within(95, 105);
+            high.should.be.within(110, 120);
+        });
     });
 });
 
