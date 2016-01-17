2168673518158ff7799ba2ed21d4c086d6e01d02
Dave McKenna <davemckenna01@gmail.com>
2016-01-16 23:14:13 -0500

    _hash returns reasonably distributed integers:green

--- hashmap/hashmap.js
+++ hashmap/hashmap.js
@@ -2,8 +2,19 @@ function HashMap() {
 
 }
 
-HashMap._hash = function () {
-    return 0;
+HashMap._hash = function (key) {
+    var value,
+        hash;
+
+    value = 0;
+
+    for (var i = 0; i < key.length; i++) {
+        value += key[i].charCodeAt();
+    }
+
+    hash = value.toString().slice(-1);
+
+    return parseInt(hash);
 }
 
 exports.HashMap = HashMap;
